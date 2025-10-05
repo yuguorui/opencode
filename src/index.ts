683b70e182316fd49e9af94822641d327b59e4d7
Dax Raad <d@ironbay.co>
2025-10-05 07:00:29 -0400

    sdk: simplify getting started with single createOpencode function
    Makes it easier for developers to get started by providing a single function that creates both server and client, removing the need to manually coordinate separate server and client creation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ export * from "./client.js"
 export * from "./server.js"
 
 import { createOpencodeClient } from "./client.js"
-import { createOpencodeServer, ServerOptions } from "./server.js"
+import { createOpencodeServer } from "./server.js"
+import type { ServerOptions } from "./server.js"
 
 export async function createOpencode(options?: ServerOptions) {
   const server = await createOpencodeServer({
