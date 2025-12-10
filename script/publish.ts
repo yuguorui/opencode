75d03e8237a6e061045eff7642317fb56ac75717
Dax Raad <d@ironbay.co>
2025-12-09 21:06:57 -0500

    sync

--- script/publish.ts
+++ script/publish.ts
@@ -19,5 +19,6 @@ for (const [key, value] of Object.entries(pkg.exports)) {
   }
 }
 await Bun.write("package.json", JSON.stringify(pkg, null, 2))
-await $`bun publish --tag ${Script.channel} --access public`
+await $`bun pm pack`
+await $`npm publish *.tgz --tag ${Script.channel} --access public`
 await Bun.write("package.json", JSON.stringify(original, null, 2))
