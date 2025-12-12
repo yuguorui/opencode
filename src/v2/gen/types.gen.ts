4e1ba97838ec8a568a12c86a53e7f56e9db85abd
GitHub Action <action@github.com>
2025-12-12 02:06:49 +0000

    chore: format code

--- src/v2/gen/types.gen.ts
+++ src/v2/gen/types.gen.ts
@@ -2407,7 +2407,7 @@ export type SessionGetResponse = SessionGetResponses[keyof SessionGetResponses]
 export type SessionUpdateData = {
   body?: {
     title?: string
-    time: {
+    time?: {
       archived?: number
     }
   }
